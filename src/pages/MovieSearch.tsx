69ce9e64157d6ae2fb19470a70e580729a7ed70a
karelch <karelch@sanezoo.com>
2020-12-19 17:02:10 +0100

    moviewindow update

--- src/pages/MovieSearch.tsx
+++ src/pages/MovieSearch.tsx
@@ -46,5 +46,95 @@ export const MovieSearch = () => {
         
         
     );
-}
+  } else {
+    selectGenre = undefined;
+  }
 
+  if (formats.indexOf("year") > -1) {
+    selectYear = (
+      <Slider
+        min={1990}
+        step={1}
+        max={2020}
+        value={yearValue}
+        onChange={handleYearChange}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        getAriaValueText={valuetext}
+      />
+    );
+  } else {
+    selectYear = undefined;
+  }
+
+  if (formats.indexOf("rating") > -1) {
+    selectRating = (
+      <Slider
+        min={0}
+        step={0.1}
+        max={5}
+        value={ratingValue}
+        onChange={handleRatingChange}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        getAriaValueText={valuetext}
+      />
+    );
+  } else {
+    selectRating = undefined;
+  }
+
+  return (
+    <div>
+      <SearchInput
+        value={searchString}
+        onClear={handleClickClear}
+        onChange={handleChangeSearchValue}
+      />
+      <Grid item sm={12} md={6}>
+        <div className={classes.toggleContainer}>
+          <ToggleButtonGroup
+            size="small"
+            value={formats}
+            onChange={handleFormat}
+            aria-label="filters"
+          >
+            <ToggleButton value="genre" aria-label="genre">
+              <PowerSettingsNew style={{ fontSize: "18px" }} />
+              &nbsp; Genre
+            </ToggleButton>
+            <ToggleButton value="rating" aria-label="rating">
+              <PowerSettingsNew style={{ fontSize: "18px" }} />
+              &nbsp; Rating
+            </ToggleButton>
+            <ToggleButton value="year" aria-label="year">
+              <PowerSettingsNew style={{ fontSize: "18px" }} />
+              &nbsp; Year
+            </ToggleButton>
+            <ToggleButton value="language" aria-label="language">
+              <PowerSettingsNew style={{ fontSize: "18px" }} />
+              &nbsp; Language
+            </ToggleButton>
+          </ToggleButtonGroup>
+
+          {selectGenre}
+          {selectRating}
+          {selectYear}
+        </div>
+      </Grid>
+      {searchString.length > 3 ? "Yes, Zobraz film!" : "No results"}
+      <MovieQueue
+        filter={{
+          rating_filter: {
+            min: 8,
+            max: 10,
+          },
+          genre_filter: {
+            needed: [],
+            forbiden: [],
+          },
+        }}
+      />
+    </div>
+  );
+};
